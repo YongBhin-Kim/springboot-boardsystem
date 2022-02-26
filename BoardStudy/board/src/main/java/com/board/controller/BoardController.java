f26739effb9b66b6ee75564f3daf68513c5de222
YongBhinKim <coji67@gmail.com>
2022-02-27 02:16:23 +0900

    Create Spring Project

---
+++ BoardStudy/board/src/main/java/com/board/controller/BoardController.java
@@ -0,0 +1,16 @@
+package com.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+    
+    @GetMapping("/")
+    @ResponseBody
+    public String main() {
+        
+        return "Hello World";
+    }
+}

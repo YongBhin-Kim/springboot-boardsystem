73a0a81565a9a305f3616ef40d5873782a479b60
YongBhinKim <coji67@gmail.com>
2022-03-01 01:57:19 +0900

    autowired 오류?

--- BoardStudy/board/src/main/java/com/board/controller/BoardController.java
+++ BoardStudy/board/src/main/java/com/board/controller/BoardController.java
@@ -1,15 +1,31 @@
 package com.board.controller;
 
+import com.board.entity.Board;
+import com.board.service.BoardService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
     
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping("/board/write") // localhost:8080/board/write
     public String boardWriteForm() {
 
         return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board) {
+
+        boardService.write(board);
+        
+        return "";
     }
 }

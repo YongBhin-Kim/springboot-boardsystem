ebbc5fd368735037683fd555f85ab2225a20f090
YongBhinKim <coji67@gmail.com>
2022-03-02 20:56:14 +0900

    write에서 boardRepository.save(board) 오류

--- BoardStudy/board/src/main/java/com/board/service/BoardService.java
+++ BoardStudy/board/src/main/java/com/board/service/BoardService.java
@@ -1,18 +1,45 @@
 package com.board.service;
 
+import java.util.List;
+
+
 import com.board.entity.Board;
 import com.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+// @Transactional
 @Service
 public class BoardService {
     
     @Autowired
     private BoardRepository boardRepository;
 
+    // 글 작성 처리
     public void write(Board board) {    
 
+        System.out.println("Service write 함수 내부 1. " + boardRepository.findAll());
         boardRepository.save(board);
+        System.out.println("Service write 함수 내부 2. " + boardRepository.findAll());
     }
+
+    
+    // 게시글 리스트 처리
+    public List<Board> boardList() {
+
+        return boardRepository.findAll();
+    }
+
+    // 특정 게시글 불러오기 
+    public Board boardView(Integer id) {
+
+        return boardRepository.findById(id).get();
+    }
+
+    // 특정 게시글 삭제
+    public void boardDelete(Integer id) {
+
+        boardRepository.deleteById(id);
+    }
+
 }

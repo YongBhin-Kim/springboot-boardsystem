4d3b03a8d9499444846b02d6c80f4c036b50e849
YongBhinKim <coji67@gmail.com>
2022-03-02 20:54:57 +0900

    write에서 boardRepository.save(board) 오류

--- BoardStudy/board/src/main/java/com/board/repository/BoardRepository.java
+++ BoardStudy/board/src/main/java/com/board/repository/BoardRepository.java
@@ -5,7 +5,8 @@ import com.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
-    
+
 }

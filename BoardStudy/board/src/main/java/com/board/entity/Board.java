b613656687f3c312cef0ef59dd2b6de6a221e5c8
YongBhinKim <coji67@gmail.com>
2022-03-03 18:52:41 +0900

    Lombok패키지설치 : @Data 인식 오류 해결

--- BoardStudy/board/src/main/java/com/board/entity/Board.java
+++ BoardStudy/board/src/main/java/com/board/entity/Board.java
@@ -1,32 +1,22 @@
 package com.board.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import lombok.Data;
 
-// @DynamicInsert
 @Entity
-// @Table(name = "board")
 @Data
 public class Board {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    // @Column(columnDefinition = "varchar(45) default 'TITLE'")
-    private String title; // = "title입니다";
+    private String title;
 
-    // @Column(columnDefinition = "TEXT default 'CONTENT'")
-    private String content;// = "content입니다";
+    private String content;
 
-
-    public void print(){
-        System.out.println("Board 내부");
-    }
 }
-

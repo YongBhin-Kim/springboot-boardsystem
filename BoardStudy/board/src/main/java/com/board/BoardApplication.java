bedf170b93e8330d2c904e61aaa08f873f698048
YongBhinKim <coji67@gmail.com>
2022-03-03 18:51:45 +0900

    main method

--- BoardStudy/board/src/main/java/com/board/BoardApplication.java
+++ BoardStudy/board/src/main/java/com/board/BoardApplication.java
@@ -2,8 +2,12 @@ package com.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.autoconfigure.domain.EntityScan;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+// @EnableJpaRepositories(basePackages = {"com.board.repository"}) // com.my.jpa.repository 하위에 있는 jpaRepository를 상속한 repository scan
+// @EntityScan(basePackages = {"com.board.entity"}) // com.my.jpa.entity 하위에 있는 @Entity 클래스 scan
 public class BoardApplication {
 
 	public static void main(String[] args) {

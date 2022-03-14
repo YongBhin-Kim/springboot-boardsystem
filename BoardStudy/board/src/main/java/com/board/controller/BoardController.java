830207483a214189383b7e9cd75c27e0fae5004d
YongBhinKim <coji67@gmail.com>
2022-03-14 23:58:26 +0900

    수정, 갱신, 메시지 띄우기(글 작성, 글 수정), 파일 업로드(글 작성, 글 수정(보류)), 페이징 처리 기능 2

--- BoardStudy/board/src/main/java/com/board/controller/BoardController.java
+++ BoardStudy/board/src/main/java/com/board/controller/BoardController.java
@@ -7,6 +7,7 @@ import com.board.entity.Board;
 import com.board.service.BoardService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
@@ -48,7 +49,16 @@ public class BoardController {
     @GetMapping("/board/list")
     public String boardList(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.DESC) Pageable pageable) {
 
-        model.addAttribute("list", boardService.boardList(pageable));
+        Page<Board> list = boardService.boardList(pageable);
+
+        int nowPage = list.getPageable().getPageNumber() + 1; // 페이징 처리 2
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
 
         return "boardlist";
     }

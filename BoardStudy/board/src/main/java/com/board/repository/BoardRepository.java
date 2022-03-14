7d5ddf07912529da361bad07ebddbd2bf54231ca
YongBhinKim <coji67@gmail.com>
2022-03-15 00:35:15 +0900

    DAO(Repository) [게시판 검색 기능 추가 : findByTitleContaining]

--- BoardStudy/board/src/main/java/com/board/repository/BoardRepository.java
+++ BoardStudy/board/src/main/java/com/board/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.board.repository;
 
 import com.board.entity.Board;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
 
+    Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
 }
